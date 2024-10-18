b28618efa03cc7007053c1e3410f6d6ee51df979
clear <clear@MacBook-Pro-Nik.local>
2024-10-18 22:54:37 +0300

    Finished hw 3.2

---
+++ homework/homework3.2.js
@@ -0,0 +1,10 @@
+const userName = "Milklizard";
+const bandName = "Chat Pile";
+const albumName = "Cool World"
+const albumScore = "8/10";
+
+const message1 = `Sup ${userName}, `;
+const message2 = `What do you think about last ${bandName} album "${albumName}"? `
+const message3 = `Anthony Fantano already rated it as ${albumScore}.`
+
+console.log(message1 + message2 + message3);
