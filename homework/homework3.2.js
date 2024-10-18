ff6761a48810698b057f2c58f71cc23e04498795
clear <clear@MacBook-Pro-Nik.local>
2024-10-18 23:37:16 +0300

    Forget to add ; in consts for HW 3.2

--- homework/homework3.2.js
+++ homework/homework3.2.js
@@ -4,7 +4,7 @@ const albumName = 'Cool World';
 const albumScore = '8/10';
 
 const message1 = `Sup ${userName}, `;
-const message2 = `What do you think about last ${bandName} album '${albumName}'? `
-const message3 = `Anthony Fantano already rated it as ${albumScore}.`
+const message2 = `What do you think about last ${bandName} album '${albumName}'? `;
+const message3 = `Anthony Fantano already rated it as ${albumScore}.`;
 
 console.log(message1 + message2 + message3);
