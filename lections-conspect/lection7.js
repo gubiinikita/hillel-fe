c55c4200d8be54bb40b1ceb5d439b6e5c8da856c
clear <clear@MacBook-Pro-Nik.local>
2024-11-02 16:11:32 +0200

    added lection 7

---
+++ lections-conspect/lection7.js
@@ -0,0 +1,52 @@
+/** Написати функцію addToArray(array, number), яка додаватиме число в масив і повертатиме новий масив, де array –
+ * вхідний масив, number – число яке потрібно додати. Якщо в масиві немає такого числа, то
+ * додати в кінець, якщо є щось нічого не робити. */
+// function addToArray(array, number) {
+//   return array.includes(number) ? array : [...array, number];
+// }
+//
+//
+// const arr = [1, 2, 3];
+// console.log(addToArray(arr, 8));
+
+/** Написати функцію generateCode(n), яка повертатиме рядок із n випадкових значень чисел від 0 до 9, де n - число. */
+// function generateCode(n) {
+//   let num = '';
+//
+//   for (let i = 0; i < n; i++) {
+//     let random = Math.floor(Math.random() * 10);
+//     num += random;
+//   }
+//
+//   return num;
+//
+// }
+// console.log(generateCode(5));
+
+/** Написати функцію sum(array), яка віддаватиме суму всіх чисел масиву. */
+// const arr = [20, 40, 77, 53];
+//
+// function sum(arr) {
+//   let total = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     total += arr[i];
+//   }
+//   return total;
+// }
+// console.log(sum(arr));
+
+/** Написати функцію лічильник. Яка буде при кожному виклику збільшувати значення на одиницю */
+function createCounter() {
+  let count = 0; // Змінна в замиканні
+
+  return function() {
+    count++; // Збільшуємо значення на 1
+    return count; // Повертаємо поточне значення
+  };
+}
+
+const counter = createCounter();
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
